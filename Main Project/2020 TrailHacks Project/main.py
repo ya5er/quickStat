9f0ee862b4004f9bd7698a3001e3d3c0111a529d
thom974 <74675902+thom974@users.noreply.github.com>
2020-12-12 14:25:16 -0500

    1

--- Main Project/2020 TrailHacks Project/main.py
+++ Main Project/2020 TrailHacks Project/main.py
@@ -1,5 +1,6 @@
 # IMPORTS
 import tkinter as tk
+from tkinter.font import Font
 from svglib.svglib import svg2rlg
 from PIL import Image, ImageTk
 from reportlab.graphics import renderPM
@@ -22,27 +23,47 @@ def main():
     root = tk.Tk()
     create_radar("Apple",[4,3,5,5,3])
     convert_svg('radarchart.svg')
-    main_window = Window(root,'quickStat','600x600')
+    main_window = Window(root,'quickStat','800x500')
 
 class Window:
     def __init__(self,root,title,size):
         self.root = root
         self.root.title(title)
         self.root.geometry(size)
-        self.bg = '#292424'
-        #self.font =
-
+        self.root.resizable(0,0)
+        self.gray = '#292424'
+        self.orange = '#E68422'
+        self.font = Font(family="MS Reference Sans Serif", size=40, weight="bold", slant="italic")
+        self.font2 = Font(family="MS Reference Sans Serif",size=16, weight="bold", slant="italic")
 
         self.design()
         self.root.mainloop()
 
-    #@staticmethod
-    #def load_font():
-    #   font = tk.
+    def handle_focus_in(self):
+        self.search_bar.delete(0, tk.END)
+        self.search_bar.config(fg='black')
+
+    def handle_focus_out(self):
+        self.search_bar.delete(0, tk.END)
+        self.search_bar.config(fg='grey')
+        self.search_bar.insert(0, "Search for players, eg. Kawhi Leonard")
+
+    def handle_enter(self):
+        print(self.search_bar.get())
+        self.handle_focus_out()
+
     def design(self):
-        self.root.configure(background=self.bg) # set bg colour on entire window
-        tk.Label(self.root, text="Welcome to QuickStat!", background=self.bg).pack()
-        tk.Entry(self.root,bd=1).pack()
+        self.root.configure(background=self.gray) # set bg colour on entire window
+        tk.Label(self.root, text="quickStat", background=self.gray, fg=self.orange, font=self.font).pack(pady=(100,35))
+        self.search_bar = tk.Entry(self.root,bd=1,width=75, relief="groove")
+        self.search_bar.pack(ipady=10)
+        self.search_bar.insert("Search for players, eg. Kawhi Leonard")
+        self.search_bar.bind("<FocusIn",self.handle_focus_in())
+        self.search_bar.bind("<FocusOut",self.handle_focus_out())
+        self.search_bar.bind("<Return",self.handle_enter())
+
+        tk.Label(self.root, text="The simplest, most relevant basketball database out there", background=self.gray, fg=self.orange, font=self.font2).pack(pady=(35,0))
+
 #----------------------------
 
 main()
--- Main Project/2020 TrailHacks Project/radar.py
+++ Main Project/2020 TrailHacks Project/radar.py
@@ -1,9 +1,7 @@
-import pygal
-from pygal.style import Style, DarkStyle
+from tkinter import *
+from tkinter import font
 
-def create_radar():
-    radar_chart = pygal.Radar(width=500,height=400, style=DarkStyle)
-    radar_chart.title = 'Comparing Businesses'
-    radar_chart.x_labels = ["Customer Service", "Prices", "Popularity", "Product Quality", "Specialty"]
-    radar_chart.add('Apple', [4,3,5,5,3])
-    radar_chart.render_to_file("Images/radarchart.svg")
+root = Tk()
+
+for font in list(font.families()):
+    print(font)
